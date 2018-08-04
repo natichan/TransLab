df2e3f64c4a41bb126767a479c0a222792685dd1
Natalia <nataliamh.88@gmail.com>
2018-08-04 01:31:53 -0400

    sign in con firebase listo

---
+++ src/js/signIn.js
@@ -0,0 +1,14 @@
+//Login
+function loginWithFirebase(){
+    const email = email.value;
+    const password = password.value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(()=>{
+            console.log("Usuario inició sesión con éxito");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+            console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
+        });
+}
