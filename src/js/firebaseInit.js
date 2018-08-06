9b505a069b3bc6981a11cb30f8cc7a93169fa2e4
Natalia <nataliamh.88@gmail.com>
2018-08-05 22:13:11 -0400

    Archivo js con iniciación de firebase

---
+++ src/js/firebaseInit.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDbzhknY6lnQXi0CYLGUePdRz9Km69IXvs",
+    authDomain: "translab-a3c8c.firebaseapp.com",
+    databaseURL: "https://translab-a3c8c.firebaseio.com",
+    projectId: "translab-a3c8c",
+    storageBucket: "translab-a3c8c.appspot.com",
+    messagingSenderId: "346982337003"
+};
+firebase.initializeApp(config);
